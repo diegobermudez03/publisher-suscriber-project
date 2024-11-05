fd6e11a6b55783d868936462789cc06276d54a81
Diego Bermudez <158313944+diegobermudez03@users.noreply.github.com>
2024-11-05 00:55:26 -0500

    System and suscriber succesfully making handshake

--- sistema.c
+++ sistema.c
@@ -10,6 +10,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include <ctype.h>
 
 
 struct Noticia{
@@ -52,30 +53,63 @@ char* GenerarInvitacion(const char not[5], int pid, struct Noticia noticias[5]) 
             }
         }
     }
+    printf("Se termino de generar invitacion\n");
     return resultado;
 }
 
 
-
 void* ManejarSuscriptores(void* arg){
-    int fd_pipe = *(int*)arg;
+    char* nombre_pipe_base = (char*)arg;
+
+    char nombre_pipe_solicitud[100];
+    snprintf(nombre_pipe_solicitud, 100, "%ssolicitud", nombre_pipe_base);
+    char nombre_pipe_respuesta[100];
+    snprintf(nombre_pipe_respuesta, 100, "%srespuesta", nombre_pipe_base);
+   
+    unlink(nombre_pipe_solicitud);
+    if (mkfifo(nombre_pipe_solicitud, 0666) == -1) {
+        perror("Error creando el pipe de solicitud");
+        exit(1);
+    }
+
+    unlink(nombre_pipe_respuesta);
+    if (mkfifo(nombre_pipe_respuesta, 0666) == -1) {
+        perror("Error creando el pipe de respuesta");
+        exit(1);
+    }
+
+    int fd_solicitud = open(nombre_pipe_solicitud, O_RDONLY);
+    if(fd_solicitud == -1){
+        perror("Error abriendo pipe de solicitud");
+        exit(1);
+    }
+
+    int fd_respuesta = open(nombre_pipe_respuesta, O_WRONLY);
+    if(fd_respuesta == -1){
+        perror("Error abriendo pipe de respuesta");
+        exit(1);
+    }
+
     char buffer_entrada[20];
 
     while(1){
         memset(buffer_entrada, 0, sizeof(buffer_entrada));
-        ssize_t bytes_read = read(fd_pipe, buffer_entrada, sizeof(buffer_entrada) - 1);
-        if (bytes_read > 0) {
-            buffer_entrada[bytes_read] = '\0';
+        ssize_t bytes_leidos = read(fd_solicitud, buffer_entrada, sizeof(buffer_entrada) - 1);
+        if (bytes_leidos > 0) {
+            printf("recibiendo solicitud %s \n", buffer_entrada);
+            buffer_entrada[bytes_leidos] = '\0';
             char letras[5];
             int pid;
             DeserializarMensajeSuscriptor(buffer_entrada, letras, &pid);
             char* mensaje = GenerarInvitacion(letras, pid, noticias);
-            write(fd_pipe, mensaje, strlen(mensaje) + 1);
+            write(fd_respuesta, mensaje, strlen(mensaje) + 1);
         }
     }
-
+    close(fd_solicitud);
+    close(fd_respuesta);
     return NULL;
 }
+
 
 
 void* ManejarPublicacion(void* arg){
@@ -88,7 +122,8 @@ void* ManejarPublicacion(void* arg){
 
 
 void* EscucharPublicadores(void* arg) {
-    int fd_pipe = *(int*)arg;
+    char* nombre_pipe = (char*)arg;
+    int fd_pipe =  open(nombre_pipe, O_RDONLY);
     char buffer_entrada[500];
     time_t tiempo_inicio, tiempo_actual;
 
@@ -98,14 +133,14 @@ void* EscucharPublicadores(void* arg) {
     time(&tiempo_inicio);
     while (1) {
         memset(buffer_entrada, 0, sizeof(buffer_entrada));
-        ssize_t bytes_read = read(fd_pipe, buffer_entrada, sizeof(buffer_entrada) - 1);
-        if (bytes_read > 0) {
-            //para reuniciar el contador
+        ssize_t bytes_leidos = read(fd_pipe, buffer_entrada, sizeof(buffer_entrada) - 1);
+        if (bytes_leidos > 0) {
+            //para reiniciar el contador
             time(&tiempo_inicio);
 
-            buffer_entrada[bytes_read] = '\0';
-            char* buffer_copia = malloc(bytes_read + 1);
-            strncpy(buffer_copia, buffer_entrada, bytes_read + 1);
+            buffer_entrada[bytes_leidos] = '\0';
+            char* buffer_copia = malloc(bytes_leidos + 1);
+            strncpy(buffer_copia, buffer_entrada, bytes_leidos + 1);
             
             pthread_t hilo_publicacion;
             pthread_create(&hilo_publicacion, NULL, ManejarPublicacion, (void*)buffer_copia);
@@ -131,7 +166,7 @@ int main(int argc, char** argsv){
     char* nombre_publicadores = NULL;
     char* nombre_suscriptores = NULL;
     int seg_espera = -1;
-    const char *pipe_prefix = "/temp/";
+    const char *pipe_prefix = "/tmp/";
     if(argc < 7){
         perror("Numero de argumentos invalido");
         exit(1);
@@ -161,33 +196,31 @@ int main(int argc, char** argsv){
     for(int i = 0; i < 5; i++){
         noticias[i].nombre = tipos_noticias[i];
         noticias[i].nombre_pipe = malloc(7);
-        noticias[i].nombre_pipe[6] = tipos_noticias[i];
-        mkfifo(noticias[i].nombre_pipe, 0666);
-        int fd_pipe = open(noticias[i].nombre_pipe, O_WRONLY);
+        snprintf(noticias[i].nombre_pipe, 7, "/tmp/%c", tipos_noticias[i]);
+        unlink(noticias[i].nombre_pipe);    //para eliminar si ya existe, por alguna razon
+        if (mkfifo(noticias[i].nombre_pipe, 0666) == -1) {
+            perror("Error creando el pipe de noticia");
+            exit(1);
+        }
+        int fd_pipe = open(noticias[i].nombre_pipe, O_RDWR);
+        if(fd_pipe == -1){
+            perror("Error abriendo  pipe para una categoria");
+            exit(1);
+        }
         noticias[i].fd_pipe = fd_pipe;
     } 
     
     //creando pipes
+    unlink(nombre_publicadores);
     mkfifo(nombre_publicadores, 0666);
-    int pub_pipe = open(nombre_publicadores, O_RDWR);
-
-    mkfifo(nombre_suscriptores, 0666);
-    int sus_pipe = open(nombre_suscriptores, O_WRONLY);
-
-    if(pub_pipe == -1 || sus_pipe == -1){
-        close(pub_pipe);
-        close(sus_pipe);
-        perror("Error creando los pipes")
-        exit(1);
-    }
 
     //creando hilo que handlea los publicadores
     pthread_t  hilo_publicadores;
-    pthread_create(&hilo_publicadores, NULL, EscucharPublicadores, (void *)&pub_pipe);
+    pthread_create(&hilo_publicadores, NULL, EscucharPublicadores, (void *)nombre_publicadores);
 
     //creando hilo que handlea los nuevos suscriptores
     pthread_t  hilo_suscriptores;
-    pthread_create(&hilo_suscriptores, NULL, ManejarSuscriptores, (void *)&sus_pipe);
+    pthread_create(&hilo_suscriptores, NULL, ManejarSuscriptores, (void *)nombre_suscriptores);
 
     //join de threads
     pthread_join(hilo_publicadores, NULL);
--- suscriptor.c
+++ suscriptor.c
@@ -10,6 +10,90 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include <unistd.h>
+
+
+void ObtenerSuscripciones(char* noticias){
+    int contador = 0;
+    while(1){
+        char ch;
+        printf("\nIngresar letra o 0: ");
+        ch = getchar();
+
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF);
+        if(ch == '0'){
+            if(noticias[0] == 'x'){
+                printf("Seleccione al menos 1 categoria\n");
+            }else{
+                break;
+            }
+        }else{
+            int ya_estaba = 0;
+            for(int i = 0; i < 5; i++){
+                if(noticias[i] == ch){
+                    printf("Ya esta seleccionada esa categoria\n");
+                    ya_estaba = 1;
+                    break;
+                }
+            }
+            if(ya_estaba == 0){
+                if(ch != 'A' && ch != 'E' && ch != 'C' && ch != 'P' && ch != 'S'){
+                    printf("Opcion  no valida\n");
+                    continue;
+                }
+                noticias[contador] = ch;
+                contador++;
+                if(contador == 5) break;
+            }
+        }
+    }
+}
+
+void ObtenerPipes(char** pipes, int fd, int pid){
+    char buffer_entrada[200];
+    while(1){
+        memset(buffer_entrada, 0, sizeof(buffer_entrada));
+        ssize_t bytes_leidos = read(fd, buffer_entrada, sizeof(buffer_entrada) - 1);
+        printf("recibiendo respuesta %s \n", buffer_entrada);
+        if (bytes_leidos > 0) {
+            buffer_entrada[bytes_leidos] = '\0';
+            int numero;
+            char* endptr;
+            numero = (int)strtol(buffer_entrada, &endptr, 10);
+            //si el numero al inicio del mensaje es nuestro pid es porque esta es la respuesta que esperabamos
+            if(numero == pid){
+                char* posicion = endptr;
+                char* pipe_temp;
+                int index = 0;
+                while ((pipe_temp = strtok(posicion, " ")) != NULL && index < 5) {
+                    posicion = NULL; 
+                    pipes[index++] = pipe_temp;
+                }
+                break;
+            }
+        }
+    }
+}
+
+void* EscucharSuscripcion(void* arg){
+    char* nombre_pipe = (char*)arg;
+    int fd = open(nombre_pipe, O_RDWR);
+    if (fd == -1) {
+      perror("Error con suscription");
+      return NULL;
+   }
+   char buffer_entrada[81];
+   while(1){
+      memset(buffer_entrada, 0, sizeof(buffer_entrada));
+      ssize_t bytes_leidos = read(fd, buffer_entrada, sizeof(buffer_entrada) - 1);
+      if (bytes_leidos > 0) {
+         buffer_entrada[bytes_leidos] = '\0';
+         printf("Recibiendo noticia: %s \n", buffer_entrada);
+      }
+   }
+   return NULL;
+}
 
 
 int main(int argc, char** argsv){
@@ -25,9 +109,19 @@ int main(int argc, char** argsv){
     }
     nombre_pipe = argsv[2];
 
-    int fd = open(nombre_pipe, O_RDWR);
-    if(fd == -1){
-        perror("Error al abrir el pipe");
+    // Crear nombres para los pipes de solicitud y respuesta
+    char nombre_pipe_solicitud[100];
+    snprintf(nombre_pipe_solicitud, 100, "/tmp/%ssolicitud", nombre_pipe);
+    char nombre_pipe_respuesta[100];
+    snprintf(nombre_pipe_respuesta, 100, "/tmp/%srespuesta", nombre_pipe);
+
+    printf("%s\n", nombre_pipe_solicitud);
+    printf("%s\n", nombre_pipe_respuesta);
+
+    // Abrir el pipe de solicitud para escritura
+    int fd_solicitud = open(nombre_pipe_solicitud, O_WRONLY);
+    if(fd_solicitud == -1){
+        perror("Error al abrir el pipe de solicitud");
         exit(1);
     }
 
@@ -40,22 +134,42 @@ int main(int argc, char** argsv){
     printf("0: YA NO MAS\n\n");
 
     int contador = 0;
-    while(1){
-        char ch;
-        printf("\nIngresar letra o 0: ");
-        ch = getchar();
-        if(ch == '0'){
-            if(noticias[0] == 'x'){
-                printf("Seleccione al menos 1 categoria\n");
-            }else{
-                break;
-            }
-        }else{
-            noticias[contador] = ch;
-            contador++;
+    ObtenerSuscripciones(noticias);
+
+    //despues de obtener todas las categorias a suscribirse entonces creamos el mensaje a enviar para suscribirnos
+    pid_t pid = getpid();
+    char* mensaje = malloc(64);
+    sprintf(mensaje, "%d", pid);
+    for (int i = 0; i < 5; i++) {
+        if (noticias[i] != 'x') {
+            strncat(mensaje, &noticias[i], 1);
         }
     }
+    write(fd_solicitud, mensaje, strlen(mensaje) + 1);
+    printf("Solicitud enviada\n");
     
+    close(fd_solicitud);
+    free(mensaje);
+
+    //Ahora esperamos a una respuesta del sistema, hasta que encontremos la que sea para nosotros, identificamos esto por el PID
+    char buffer_entrada[200];
+    char* pipes_suscripciones[5] = {"x", "x", "x", "x", "x"};
+
+    // Abrir el pipe de respuesta para lectura
+    int fd_respuesta = open(nombre_pipe_respuesta, O_RDONLY);
+    if(fd_respuesta == -1){
+        perror("Error al abrir el pipe de respuesta");
+        exit(1);
+    }
+    ObtenerPipes(pipes_suscripciones, fd_respuesta, pid);
+    close(fd_respuesta);
+
+    for(int i = 0; i < 5; i++){
+        if(strcmp(pipes_suscripciones[i],"x") != 0){
+            pthread_t  hilo_tema;
+            pthread_create(&hilo_tema, NULL, EscucharSuscripcion, (void *)&pipes_suscripciones[i]);
+        }
+    }
 
     return 0;
 }
