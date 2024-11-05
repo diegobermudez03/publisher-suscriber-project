cae13ebec88231780c30221d71b54698067438c2
Diego Bermudez <158313944+diegobermudez03@users.noreply.github.com>
2024-11-05 14:23:42 -0500

    Change made in publisher since it turns out that the publisher had to first verify that all the news follow the structure required

--- publicador.c
+++ publicador.c
@@ -12,6 +12,7 @@
 #include <sys/wait.h>
 #include <ctype.h>
 
+#define MAX_LINEAS 100
 
 int segundos_esperar;
 
@@ -51,10 +52,6 @@ int main(int argc, char** argsv){
         exit(1);
     }
 
-    //enviar mensaje "abierto" por el pipe, esto indica al sistema que se unio un publicador
-    char* mensaje_abierto = "abierto";
-    write(fd_pipe, mensaje_abierto, strlen(mensaje_abierto) + 1);
-
     //abriendo archivo para lectura
     FILE* archivo = fopen(nombre_archivo, "r");
     if (archivo == NULL) {
@@ -65,22 +62,47 @@ int main(int argc, char** argsv){
     char* linea = NULL;
     size_t longitud = 0;
     ssize_t leido;
+    char* lineas_validas[MAX_LINEAS];
+    int total_lineas = 0;
 
-    //se lee el archivo linea a linea y se envian al sistema, esperando entre noticia
-    //y noticia el tiempo estipulado por la bandera
+    //leer el archivo linea a linea y verificar que el formato sea el correcto
     while ((leido = getline(&linea, &longitud, archivo)) != -1) {
-        write(fd_pipe, linea, leido + 1);
-        printf("\nNoticia enviada %s\n", linea);
-        sleep(segundos_esperar);
+        //se verifica la letra inicial y los :
+        if ((linea[0] == 'A' || linea[0] == 'P' || linea[0] == 'E' || linea[0] == 'S' || linea[0] == 'C') && linea[1] == ':' && strlen(linea) <= 83) {
+            //Si la linea es valida, entonces se añade a las lineas validads
+            lineas_validas[total_lineas] = malloc(strlen(linea) + 1);
+            strcpy(lineas_validas[total_lineas], linea);
+            total_lineas++;
+        } else {
+            //si la linea no es valida, entonces se muestra el error y finaliza la ejecucion
+            perror("El archivo no cumple con el formato");
+            free(linea);
+            fclose(archivo);
+            close(fd_pipe);
+            exit(1);
+        }
     }
 
-    //luego de leer todo el archivo se envia un mensaje de cerrado al sistema
-    //esto es para que el sepa que un publicador acaba de terminar, y pueda llevar control de si hay publicadores vivos
+    fclose(archivo);
+    free(linea);
+
+    //enviar mensaje "abierto" por el pipe, esto indica al sistema que se unio un publicador
+    char* mensaje_abierto = "abierto";
+    write(fd_pipe, mensaje_abierto, strlen(mensaje_abierto) + 1);
+
+    //se envian todas las lineas por el pipe, por cada linea se duerme el tiempo estipulado por la flag
+    for (int i = 0; i < total_lineas; i++) {
+        write(fd_pipe, lineas_validas[i], strlen(lineas_validas[i]) + 1);
+        printf("\nNoticia enviada %s\n", lineas_validas[i]);
+        sleep(segundos_esperar);
+        free(lineas_validas[i]);
+    }
+
+    //luego de enviar todas las noticias se envia un mensaje de cerrado
+    // esto es para que el sepa que un publicador acaba de terminar, y pueda llevar control de si hay publicadores vivos
     char* mensaje_cerrado = "cerrado";
     write(fd_pipe, mensaje_cerrado, strlen(mensaje_cerrado) + 1);
 
-    free(linea);
-    fclose(archivo);
     close(fd_pipe);
 
     return 0;
